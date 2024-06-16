aec10eee4215ff8b87511fa405b89b3912af3ff4
tinaandpython <150164664+tinaandpython@users.noreply.github.com>
2024-06-16 19:42:34 +0300

    completed front-end, customized forms, built docker container

--- trip_planner/itinerary_generator/forms.py
+++ trip_planner/itinerary_generator/forms.py
@@ -1,9 +1,16 @@
 from django import forms
 
-
 class ItineraryGeneratorForm(forms.Form):
-    destination = forms.CharField(max_length=255, label='Destination')
-    days = forms.IntegerField(min_value=1, label='Number of days')
+    destination = forms.CharField(
+        max_length=255,
+        label='Destination',
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    days = forms.IntegerField(
+        min_value=1,
+        label='Number of days',
+        widget=forms.NumberInput(attrs={'class': 'form-control'})
+    )
 
 
 
